b00f900f42870dd3a00eb174222061ac85ea04cf
Chongsawad Saiaram <chongsawad252@gmail.com>
2012-05-14 15:11:57 +0700

    edit label

---
+++ OpenAlarm/ODEditLabelViewController.m
@@ -0,0 +1,79 @@
+//
+//  ODEditLabelViewController.m
+//  OpenAlarm
+//
+//  Created by In|Ce Saiaram on 5/14/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "ODEditLabelViewController.h"
+
+@interface ODEditLabelViewController ()
+
+@end
+
+@implementation ODEditLabelViewController
+@synthesize textLabel;
+@synthesize delegate;
+@synthesize currentName;
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    UIBarButtonItem *cancelButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemCancel target:self action:@selector(cancel:)];
+    self.navigationItem.leftBarButtonItem = cancelButton;
+    
+    UIBarButtonItem *saveButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemSave target:self action:@selector(save:)];
+    self.navigationItem.rightBarButtonItem = saveButton;
+    self.textLabel.text = currentName;
+    
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    
+    [self.textLabel becomeFirstResponder];
+}
+
+- (void)viewDidDisappear:(BOOL)animated
+{
+    [super viewDidDisappear:animated];
+    
+    [self.textLabel resignFirstResponder];
+}
+
+- (void)viewDidUnload
+{
+    [self setTextLabel:nil];
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (void)save:(id)sender
+{
+    [self.delegate shouldSaveTextLabel:self :textLabel.text];
+    [self.navigationController popViewControllerAnimated:YES];
+}
+
+- (void)cancel:(id)sender
+{
+    [self.navigationController popViewControllerAnimated:YES];
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
